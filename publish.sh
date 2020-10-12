9f80138b5a78f018c07c19a282ad25ab630ecb83
Joe Ford <ford153focus@gmail.com>
2020-10-12 13:52:39 +0300

    migrate to .net5

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 # time dotnet publish --output bin/Release/linux-x64 --configuration Release --runtime linux-x64 --self-contained --verbosity minimal /p:PublishSingleFile=true /p:PublishTrimmed=true
 # time dotnet publish --output bin/Release/win10-x64 --configuration Release --runtime win10-x64 --self-contained --verbosity minimal /p:PublishSingleFile=true /p:PublishTrimmed=true
-time dotnet publish --output bin/Release/linux-x64 --configuration Release --runtime linux-x64 --self-contained --verbosity minimal /p:PublishTrimmed=true
+# time dotnet publish --output bin/Release/linux-x64 --configuration Release --runtime linux-x64 --self-contained --verbosity minimal /p:PublishTrimmed=true
+cd ~/Workspace/mailfilter && time ~/.local/opt/dotnet/dotnet publish --output bin/Release/linux-x64 --configuration Release --framework "net5.0" --runtime linux-x64 --self-contained --verbosity minimal /p:PublishTrimmed=true
 # time dotnet publish --output bin/Release/win10-x64 --configuration Release --runtime win10-x64 --self-contained --verbosity minimal /p:PublishTrimmed=true
