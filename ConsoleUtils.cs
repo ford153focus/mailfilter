4f6de6ce88620c456846fdf5485e525a49dd0fd6
Joe Ford <ford153focus@gmail.com>
2021-02-04 20:02:03 +0300

    Split utils

---
+++ ConsoleUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace MailFilter
+{
+    public class ConsoleUtils
+    {
+        public static void WriteError(string message)
+        {
+            Console.OutputEncoding = Encoding.Default;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(message);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        public static void WriteSuccess(string message)
+        {
+            Console.OutputEncoding = Encoding.Default;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(message);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        public static void WriteWarning(string message)
+        {
+            Console.OutputEncoding = Encoding.Default;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(message);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+}
--- WrappedMessage.cs
+++ WrappedMessage.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using MailKit;
@@ -34,6 +35,44 @@ namespace MailFilter
 
             MailAddress senderAddressObj = new MailAddress(SenderAddress ?? string.Empty);
             Host = senderAddressObj.Host;
+        }
+
+        public void Delete()
+        {
+            Inbox.AddFlags(Index, MessageFlags.Deleted, true);
+            Inbox.Expunge();
+            ConsoleUtils.WriteError("deleted");
+        }
+
+        public void Move(string outputMessage, List<string> targetFolder)
+        {
+            try
+            {
+                // Get subfolder
+                // Try to create subfolder on error
+                IMailFolder currentFolder = Client.GetFolder(Client.PersonalNamespaces[0]);
+                foreach (var folder in targetFolder)
+                {
+                    try
+                    {
+                        currentFolder = currentFolder.GetSubfolder(folder);
+                    }
+                    catch (Exception)
+                    {
+                        currentFolder = currentFolder.Create(folder, true);
+                    }
+                }
+
+                // Move
+                Inbox.MoveTo(Index, currentFolder);
+                Inbox.Expunge();
+
+                ConsoleUtils.WriteSuccess(outputMessage);
+            }
+            catch (Exception e)
+            {
+                ConsoleUtils.WriteError(e.ToString());
+            }
         }
     }
 }
