56bb3ec2ed0100930d747578b3c148553ab92f80
Joe Ford <ford153focus@gmail.com>
2022-04-22 13:54:37 +0300

    new filter

---
+++ Filters/Spam.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+
+namespace MailFilter.Filters
+{
+    internal class Spam
+    {
+        public static void Filter(WrappedMessage wMsg)
+        {
+            if (wMsg.Message.To.Count > 3) {
+                if (wMsg.Message.Attachments.Any()) {
+                    var attachment = wMsg.Message.Attachments.First();
+                    var a_fn = ((MimeKit.MimePart)attachment).FileName;
+                    if (a_fn.EndsWith(".pdf")) {
+                        wMsg.Delete();
+                    }
+                }
+            }
+        }
+    }
+}
