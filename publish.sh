ab2f8ef42984a280349f14b1a763cf2e8fa63dd6
Joe Ford <ford153focus@gmail.com>
2022-07-07 16:26:55 +0300

    fix paths

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 # time dotnet publish --output bin/Release/linux-x64 --configuration Release --runtime linux-x64 --self-contained --verbosity minimal /p:PublishSingleFile=true /p:PublishTrimmed=true -p:PublishReadyToRun=true
 # time dotnet publish --output bin/Release/win10-x64 --configuration Release --runtime win10-x64 --self-contained --verbosity minimal /p:PublishSingleFile=true /p:PublishTrimmed=true -p:PublishReadyToRun=true
-cd ~/Workspace/mailfilter && time ~/.local/opt/dotnet6/dotnet publish --output bin/Release/linux-x64 --configuration Release --runtime linux-x64 --self-contained -p:PublishReadyToRun=true
+time dotnet publish --output bin/Release/linux-x64 --configuration Release --runtime linux-x64 --self-contained -p:PublishReadyToRun=true
