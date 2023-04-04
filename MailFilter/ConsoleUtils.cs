def4b9f71c3c9e3d84b2f743d190a4a7066c40d9
Joe Ford <ford153focus@gmail.com>
2023-04-04 21:04:54 +0300

    fix 4 multithreading

--- MailFilter/ConsoleUtils.cs
+++ MailFilter/ConsoleUtils.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Text;
 
-namespace MailFilter
+namespace MailFilter;
+
+public class ConsoleUtils
 {
-    public static class ConsoleUtils
+    private static readonly object ConsoleLock = new object();
+
+    public static void WriteError(string message)
     {
-        public static void WriteError(string message)
+        lock (ConsoleLock)
         {
             Console.OutputEncoding = Encoding.Default;
             Console.BackgroundColor = ConsoleColor.DarkRed;
@@ -13,9 +17,13 @@ namespace MailFilter
             Console.Write(message);
             Console.ResetColor();
             Console.WriteLine();
+            Console.WriteLine();
         }
+    }
 
-        public static void WriteSuccess(string message)
+    public static void WriteSuccess(string message)
+    {
+        lock (ConsoleLock)
         {
             Console.OutputEncoding = Encoding.Default;
             Console.BackgroundColor = ConsoleColor.Green;
@@ -23,15 +31,20 @@ namespace MailFilter
             Console.Write(message);
             Console.ResetColor();
             Console.WriteLine();
+            Console.WriteLine();
         }
+    }
 
-        public static void WriteWarning(string message)
+    public static void WriteWarning(string message)
+    {
+        lock (ConsoleLock)
         {
             Console.OutputEncoding = Encoding.Default;
             Console.BackgroundColor = ConsoleColor.Yellow;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Write(message);
             Console.ResetColor();
+            Console.WriteLine();
             Console.WriteLine();
         }
     }
